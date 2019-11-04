616de4a47496d35415760f4e625a83e501682e72
mypyn11 <mypyn11@gmail.com>
2019-11-04 13:04:14 +0900

    ヘッダーフッターリンク追加

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,13 +1,13 @@
 <footer class="zf-footer">
     <ul class="footer__menu">
-        <li><a href="">会社概要</a></li>
-        <li><a href="">代表者挨拶</a></li>
-        <li><a href="">宿泊事業部</a></li>
-        <li><a href="">不動産事業部</a></li>
-        <li><a href="">ニュースTOP</a></li>
-        <li><a href="">採用情報TOP</a></li>
-        <li><a href="">お問い合わせ</a></li>
-        <li><a href="">プライバシポリシー</a></li>
+        <li><a href="/company">会社概要</a></li>
+        <li><a href="/greeting">代表者挨拶</a></li>
+        <li><a href="/accommodation_division">宿泊事業部</a></li>
+        <li><a href="/estate_division">不動産事業部</a></li>
+        <li><a href="/news">ニュースTOP</a></li>
+        <li><a href="/recruit">採用情報TOP</a></li>
+        <li><a href="/contact">お問い合わせ</a></li>
+        <li><a href="/privacy_policy">プライバシポリシー</a></li>
     </ul>
     <p class="uf-txt-ctr">©2019 CompanyInformation Inc.</p>
 </footer>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,14 +5,14 @@
         <label class="nav-unshown" id="nav-close" for="nav-input"></label>
         <div id="nav-content">
             <label id="nav-close-btn" class="cancel-btn" for="nav-input"></label>
-            <a href="">会社概要</a>
-            <a href="">代表者挨拶</a>
-            <a href="">宿泊事業部</a>
-            <a href="">不動産事業部</a>
-            <a href="">ニュースTOP</a>
-            <a href="">採用情報TOP</a>
-            <a href="">お問い合わせ</a>
-            <a href="">プライバシポリシー</a>
+            <a href="/company">会社概要</a>
+            <a href="/greeting">代表者挨拶</a>
+            <a href="/accommodation_division">宿泊事業部</a>
+            <a href="/estate_division">不動産事業部</a>
+            <a href="/news">ニュースTOP</a>
+            <a href="/recruit">採用情報TOP</a>
+            <a href="/contact">お問い合わせ</a>
+            <a href="/privacy_policy">プライバシポリシー</a>
         </div>
     </div>
 
@@ -20,14 +20,14 @@
     </div>
     <div class="header__menu">
         <ul>
-            <li><a href="">会社概要</a></li>
-            <li><a href="">代表者挨拶</a></li>
-            <li><a href="">宿泊事業部</a></li>
-            <li><a href="">不動産事業部</a></li>
-            <li><a href="">ニュースTOP</a></li>
-            <li><a href="">採用情報TOP</a></li>
-            <li><a href="">お問い合わせ</a></li>
-            <li><a href="">プライバシポリシー</a></li>
+            <li><a href="/company">会社概要</a></li>
+            <li><a href="/greeting">代表者挨拶</a></li>
+            <li><a href="/accommodation_division">宿泊事業部</a></li>
+            <li><a href="/estate_division">不動産事業部</a></li>
+            <li><a href="/news">ニュースTOP</a></li>
+            <li><a href="/recruit">採用情報TOP</a></li>
+            <li><a href="/contact">お問い合わせ</a></li>
+            <li><a href="/privacy_policy">プライバシポリシー</a></li>
         </ul>
     </div>
 </header>
